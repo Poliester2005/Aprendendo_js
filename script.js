120a521411152e2cff66b6d6f83c9d50694a22a6
Eduardo Pielich <107966984+Poliester2005@users.noreply.github.com>
2023-03-09 21:02:19 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,101 @@
+function mudaCor() {
+    var cor = Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = '#' + cor
+
+    var align = (Math.floor(Math.random() * 3))
+
+    switch (align) {
+        case 0:
+            document.body.style.textAlign = 'center'
+            break
+        case 1:
+            document.body.style.textAlign = 'left'
+            break
+        case 2:
+            document.body.style.textAlign = 'right'
+            break
+    }
+
+}
+
+function add() {
+    var num = parseInt(document.getElementById('num').innerHTML)
+    document.getElementById('num').innerHTML = num + 1
+    num = parseInt(document.getElementById('num').innerHTML)
+
+    if (num > 0) {
+        document.getElementById('num').style.color = 'green'
+    } else if (num < 0) {
+        document.getElementById('num').style.color = 'red'
+    } else if (num = 0) {
+        document.getElementById('num').style.color = 'yellow'
+    }
+}
+
+function sub() {
+    var num = parseInt(document.getElementById('num').innerHTML)
+    document.getElementById('num').innerHTML = num - 1
+    num = parseInt(document.getElementById('num').innerHTML)
+
+
+    if (num > 0) {
+        document.getElementById('num').style.color = 'green'
+    } else if (num < 0) {
+        document.getElementById('num').style.color = 'red'
+    } else if (num == 0) {
+        document.getElementById('num').style.color = 'yellow'
+    }
+    console.log
+}
+
+function sobre() {
+    document.getElementById('sobre').style.display = 'block'
+    document.getElementById('text').style.display = 'none'
+    document.getElementById('Counter').style.display = 'none'
+    document.getElementById('ppt').style.display = 'none'
+}
+
+function text() {
+    document.getElementById('sobre').style.display = 'none'
+    document.getElementById('text').style.display = 'block'
+    document.getElementById('Counter').style.display = 'none'
+    document.getElementById('ppt').style.display = 'none'
+}
+
+function counter() {
+    document.getElementById('sobre').style.display = 'none'
+    document.getElementById('text').style.display = 'none'
+    document.getElementById('Counter').style.display = 'block'
+    document.getElementById('ppt').style.display = 'none'
+}
+
+function ppt() {
+    document.getElementById('sobre').style.display = 'none'
+    document.getElementById('text').style.display = 'none'
+    document.getElementById('Counter').style.display = 'none'
+    document.getElementById('ppt').style.display = 'block'
+}
+
+function pedra() {
+    var ia = Math.floor(Math.random() * 3)
+
+    switch (ia) {
+        case 0:
+            document.getElementById('ia').innerHTML = 'papel'
+            document.getElementById('player').innerHTML = 'pedra'
+            document.getElementById('resultado').innerHTML = 'Você perdeu'
+            break
+        case 1:
+            document.getElementById('ia').innerHTML = 'pedra'
+            document.getElementById('player').innerHTML = 'pedra'
+            document.getElementById('resultado').innerHTML = 'Empate'
+            break
+        case 2:
+            document.getElementById('ia').innerHTML = 'Tesoura'
+            document.getElementById('player').innerHTML = 'pedra'
+            document.getElementById('resultado').innerHTML = 'Você venceu'
+            break
+    }
+
+    console.log(ia)
+}
